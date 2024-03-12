e7fcd27486dcf0ad535a30e33a3c92599f5c8fdb
TKTY612314 <88832283+tkna3963@users.noreply.github.com>
2024-03-12 14:27:36 +0900

    Early_Est追加と見た目変更

--- main_script.js
+++ main_script.js
@@ -69,8 +69,8 @@ function formatDateTimeForUrl(dateTime) {
 
 function yahooShingenn() {
     const currentDateTime = new Date();
-    //const currentDateTime = new Date(2024, 0, 1, 16, 11,5);
-    const fiveSecondsAgo = new Date(currentDateTime.getTime() - 5 * 1000);
+    //const currentDateTime = new Date(2024, 0, 1, 16,10,0);
+    const fiveSecondsAgo = new Date(currentDateTime.getTime() - 0 * 1000);
     const time_set = formatDateTimeForUrl(fiveSecondsAgo);
     const apiUrl = `https://weather-kyoshin.west.edge.storage-yahoo.jp/RealTimeData/${time_set}.json`;
     const xhr = new XMLHttpRequest();
@@ -82,7 +82,7 @@ function yahooShingenn() {
         if (yahoo_data.hypoInfo === null) {
             const strongEarthquake = yahoo_data.realTimeData.intensity;
             const maxstrongEarthquake = Math.max(...strongEarthquake.split('').map(char => seismicIntensityConversion(char)));
-            return [currentDateTime,maxstrongEarthquake, apiUrl,"緊急地震速報は出ていません。",0,0, 0, 0,36.0,137.9, 0, 0];
+            return [currentDateTime, maxstrongEarthquake, apiUrl, "緊急地震速報は出ていません。", 0, 0, 0, 0, 36.0, 137.9, 0, 0];
         } else {
             const reportNum = yahoo_data.hypoInfo.items[0].reportNum;
             const isFinal = yahoo_data.hypoInfo.items[0].isFinal;
@@ -96,7 +96,12 @@ function yahooShingenn() {
             const Wave_longitude = yahoo_data.psWave.items[0].longitude.replace("E", "");
             const pRadius = parseFloat(yahoo_data.psWave.items[0].pRadius).toFixed(0);
             const sRadius = parseFloat(yahoo_data.psWave.items[0].sRadius).toFixed(0);
-            const report = isFinal ? `最終報(第${reportNum}報)` : `第${reportNum}報`;
+            let report;
+            if (isFinal === true) {
+                report = `最終報(第${reportNum}報)`;
+            } else {
+                report = `第${reportNum}報`;
+            }
             return [currentDateTime, maxstrongEarthquake, apiUrl, report, regionName, calcIntensity, magnitude, depth, Wave_latitude, Wave_longitude, pRadius, sRadius];
         }
     } else {
